4a2fe0b93ce4f4f0fa59d9c2f2ae846d59530365
Alexandre Sparton <sparton.alexandre@gmail.com>
2024-01-09 11:05:54 +0100

    feat(command-id): command id enum added to uniquely identify commands

--- src/command_parser/command.rs
+++ src/command_parser/command.rs
@@ -1,8 +1,9 @@
-use crate::error::parse_command_error::HelpDisplay;
+use crate::{commands_collections::FOLIO_COMMAND_ID, error::parse_command_error::HelpDisplay};
 
 /// Identify a folio CLI command.
 #[derive(Clone)]
 pub struct FolioCommand {
+    id: FOLIO_COMMAND_ID,
     name: String,
     description: String,
     verify_args_func: fn(&Vec<String>) -> bool,
@@ -10,15 +11,21 @@ pub struct FolioCommand {
 
 impl FolioCommand {
     pub fn new(
+        id: FOLIO_COMMAND_ID,
         name: String,
         description: String,
         verify_args_func: fn(&Vec<String>) -> bool,
     ) -> FolioCommand {
         FolioCommand {
+            id,
             name,
             description,
             verify_args_func,
         }
+    }
+
+    pub fn get_id(&self) -> FOLIO_COMMAND_ID {
+        self.id.clone()
     }
 
     pub fn get_name(&self) -> String {
