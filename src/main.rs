c8059165c6e6f778ab14a03ce7e8afda1566b676
Alexandre Sparton <sparton.alexandre@gmail.com>
2024-01-09 12:36:41 +0100

    fix(main): token leaked... Oupsi! Don't worry I changed it :)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use controllers::project_controller::get_project_of_remote;
-
 mod command_parser;
 mod commands_collections;
 mod controllers;
@@ -8,16 +6,4 @@ mod gh_reqwestor;
 mod gh_token_getter;
 mod input_getter;
 
-#[tokio::main]
-async fn main() {
-    match get_project_of_remote(
-        &"asparton-portfolio".to_string(),
-        &"OpenFlappy".to_string(),
-        &"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",
-    )
-    .await
-    {
-        Err(_error) => println!("Invalid remote or project name."),
-        Ok(project) => println!("{:?}", project),
-    }
-}
+fn main() {}
