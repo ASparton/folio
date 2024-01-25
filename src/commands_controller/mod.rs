5155ce4c69d24fd98c8f7b3ea837340966395e14
Alexandre Sparton <sparton.alexandre@gmail.com>
2024-01-25 20:35:13 +0100

    feat(remote-list-display): remote list displayed inside table + useless model deleted

--- src/commands_controller/mod.rs
+++ src/commands_controller/mod.rs
@@ -1,23 +1,23 @@
 use crate::command_parser::CommandsCollection;
-use crate::commands_collections::FOLIO_COMMAND_ID;
+use crate::commands_collections::FolioCommandId;
 use crate::error::folio_error::FolioError;
 
 mod project_controller;
 mod remote_controller;
 
 pub async fn execute_command(
-    command_id: &FOLIO_COMMAND_ID,
+    command_id: &FolioCommandId,
     commands_collection: &CommandsCollection,
     gh_auth_token: &String,
     _args: &Vec<String>,
 ) -> Result<(), FolioError> {
     match command_id {
-        FOLIO_COMMAND_ID::RemoteHelp => remote_controller::display_remote_help(commands_collection),
-        FOLIO_COMMAND_ID::RemoteList => remote_controller::list_remotes(gh_auth_token).await,
-        FOLIO_COMMAND_ID::ProjectHelp => {
+        FolioCommandId::RemoteHelp => remote_controller::display_remote_help(commands_collection),
+        FolioCommandId::RemoteList => remote_controller::list_remotes(gh_auth_token).await,
+        FolioCommandId::ProjectHelp => {
             project_controller::display_project_help(commands_collection)
         }
-        FOLIO_COMMAND_ID::ProjectList => project_controller::list_projects(),
-        FOLIO_COMMAND_ID::ProjectView => project_controller::view_project(),
+        FolioCommandId::ProjectList => project_controller::list_projects(),
+        FolioCommandId::ProjectView => project_controller::view_project(),
     }
 }
